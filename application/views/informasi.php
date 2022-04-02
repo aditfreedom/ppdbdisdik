bcc640fd6919e18e5f3b22b24c0f28429173a6df
Adit <37005476+aditfreedom@users.noreply.github.com>
2022-04-03 00:54:39 +0700

    Final

--- application/views/informasi.php
+++ application/views/informasi.php
@@ -22,7 +22,6 @@
 
   <!-- Favicons -->
   <link href="<?=base_url()?>assets/img/favicon.png" rel="icon">
-  <link href="<?=base_url()?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
